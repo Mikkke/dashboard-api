74efd773817f3b29e65d351b415db67414be68a7
Mike <jkellymichael@gmail.com>
2021-03-25 13:14:21 +0100

    create token middleware

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ const userRouter = require("express").Router();
 const bcryp = require("bcrypt");
 const { User } = require("../models");
 const { v4: uuid } = require("uuid");
+const { createTokens, validateTokens } = require("../middleware/jwt");
 
 userRouter.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
@@ -20,7 +21,23 @@ userRouter.post("/register", async (req, res) => {
     res.status(400).json({ msg: error });
   }
 });
-userRouter.post("/login", (req, res) => {});
+userRouter.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const userExist = await User.findOne({ where: { email: email } });
+  if (!userExist) {
+    res.status(400).json({ msg: "not user with that email" });
+  }
+  const dbPassword = userExist.password;
+  const comparePassword = await bcryp.compare(password, dbPassword);
+  if (!comparePassword) {
+    res.status(400).json({ msg: "wrong email or password" });
+  } else {
+    const accessToken = createTokens(userExist);
+    res.cookie("access-token", accessToken, { maxAge: 60 * 60 });
+    res.json({ msg: "you are logged in" });
+  }
+});
+
 userRouter.get("/getprofil", (req, res) => {
   res.send("good from api");
 });
